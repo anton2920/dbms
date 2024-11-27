300ed66f97cc33c55d8c80c002607f0aca3b384a
anton2920 <anton2920@gmail.com>
2024-11-27 22:09:40 +0300

    Added benchmark for zig-zag number pattern

--- btree_test.go
+++ btree_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
+
+	"github.com/anton2920/gofa/util"
 )
 
 const Seed = 100500
@@ -19,7 +21,7 @@ func BenchmarkRandInt(b *testing.B) {
 
 func BenchmarkBtreeSet(b *testing.B) {
 	const MinOrder = 2
-	const MaxOrder = 16
+	const MaxOrder = 128
 
 	b.Run("Random", func(b *testing.B) {
 		for order := MinOrder; order <= MaxOrder; order++ {
@@ -62,6 +64,20 @@ func BenchmarkBtreeSet(b *testing.B) {
 			})
 		}
 	})
+	b.Run("Zig-zag", func(b *testing.B) {
+		for order := MinOrder; order <= MaxOrder; order++ {
+			b.Run(fmt.Sprintf("Order-%d", order), func(b *testing.B) {
+				var number int
+				var bt Btree
+
+				bt.Order = order
+				for i := 0; i < b.N; i++ {
+					bt.Set(K(number), 0)
+					number = -number + 1*(-1*util.Bool2Int(number >= 0))
+				}
+			})
+		}
+	})
 }
 
 func BenchmarkMapSet(b *testing.B) {
@@ -89,6 +105,15 @@ func BenchmarkMapSet(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			m[K(number)] = 0
 			number--
+		}
+	})
+	b.Run("Zig-zag", func(b *testing.B) {
+		var number int
+		m := make(map[K]V)
+
+		for i := 0; i < b.N; i++ {
+			m[K(number)] = 0
+			number = -number + 1*(-1*util.Bool2Int(number >= 0))
 		}
 	})
 }
